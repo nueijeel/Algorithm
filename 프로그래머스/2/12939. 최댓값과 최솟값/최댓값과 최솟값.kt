d998c34078d1a01e40000e3b9f7f1d8ac1e783af
이지은 <106245278+nueijeel@users.noreply.github.com>
2025-06-06 13:25:39 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 38.15 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
@@ -0,0 +1,9 @@
+import kotlin.math.*
+
+class Solution {
+    fun solution(s: String): String {
+        var list = s.split(" ").map { it.toInt() }
+        var newList = list.sorted()
+        return "${newList[0]} ${newList[newList.size - 1]}"
+    }
+}
