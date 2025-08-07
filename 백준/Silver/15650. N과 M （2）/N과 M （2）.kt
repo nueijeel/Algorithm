05712cbce92855e1f03aeaf8c958dd3e93d31503
이지은 <lge6716@gmail.com>
2025-08-07 14:46:24 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 21328 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
@@ -0,0 +1,32 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+    val sb = StringBuilder()
+    
+    backtrack(sb, n, m, mutableListOf<Int>(), BooleanArray(n + 1))
+    
+    print(sb)
+}
+
+fun backtrack(sb: StringBuilder, n: Int, m: Int, path: MutableList<Int>, used: BooleanArray) {
+    if (path.size == m) {
+        for (i in path.indices) {
+            sb.append(path[i])
+            if (i != m - 1) sb.append(" ")
+        }
+        sb.appendLine()
+        return
+    }
+    
+    for (i in 1 .. n) {
+        if (path.size > 0 && path[path.lastIndex] > i) continue
+        if (!used[i]) {
+            path.add(i)
+            used[i] = true
+            
+            backtrack(sb, n, m, path, used)
+            
+            path.removeAt(path.lastIndex)
+            used[i] = false
+        }
+    }
+}
